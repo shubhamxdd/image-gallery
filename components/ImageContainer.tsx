d4032138d06fccbda6b71e47dcee3dd463374cc9
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-09-28 12:55:08 +0530

    Add classes and sizes

--- components/ImageContainer.tsx
+++ components/ImageContainer.tsx
@@ -8,8 +8,17 @@ type Props = {
 const ImageContainer = ({ photo }: Props) => {
   return (
     <>
-      <div key={photo.id} className="h-64 bg-gray-200 rounded-xl">
-        <Image src={photo.src.large} alt={photo.alt} width={250} height={250} />
+      <div
+        key={photo.id}
+        className="h-64 bg-gray-200 rounded-xl relative overflow-hidden group"
+      >
+        <Image
+          src={photo.src.large}
+          alt={photo.alt}
+          fill
+          className="object-cover group-hover:opacity-75"
+          sizes="(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
+        />
       </div>
     </>
   );
